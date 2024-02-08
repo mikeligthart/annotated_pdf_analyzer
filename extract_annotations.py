3edfb761bfc5cb6a23e95c55e44953e69b10f9b4
DanilaRsn <danilarusinkiewicz@hotmail.com>
2024-02-08 22:18:31 +0100

    Worked on issues #1 #2 #4 #5 #6. There is now an initial solution for all issues up until #6.

--- extract_annotations.py
+++ extract_annotations.py
@@ -6,14 +6,23 @@ from typing import List, Tuple
 import fitz
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import nltk
+import spacy
 nltk.download('stopwords')
 nltk.download('punkt')
+import spacy
+import re
+import spacy.cli
+#spacy.cli.download("en_core_web_sm")
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+
 
 
 
@@ -45,12 +54,48 @@ class ExtractAnnotationsFromPDF:
 
         highlights = []
         comments = []
-        for page in doc:
+       # for page in doc:
+        # for page_num in range(2, doc.page_count):
+        #     page = doc.load_page(page_num)
+
+        #     highlights += self._extract_highlights_from_page(page)  # extract highlighted text
+        #     for annot in page.annots():
+        #         comments.append(annot.info["content"])  # extract text from comments
+
+        for page_num in range(2, doc.page_count):
+            page = doc.load_page(page_num)
+
             highlights += self._extract_highlights_from_page(page)  # extract highlighted text
-            for annot in page.annots():
+            annotations = [(annot, annot.rect.y0) for annot in page.annots()]  # Get annotations with their vertical positions
+            annotations.sort(key=lambda x: x[1])  # Sort annotations based on their vertical positions
+
+            for annot, _ in annotations:
                 comments.append(annot.info["content"])  # extract text from comments
 
-        return list(zip_longest(highlights, comments))
+        data = list(zip_longest(highlights, comments)) #old return
+
+        cleaned_data = []
+        i = 0
+        while i < len(data):
+            sentence, highlight = data[i]
+            if sentence != '' and highlight == '':
+                if i + 1 < len(data):
+                    next_sentence, next_highlight = data[i + 1]
+                    if next_sentence == '' and next_highlight != '':
+                        cleaned_data.append((sentence, next_highlight))
+                        i += 1  # skip the next sentence and highlight as they have been combined
+            elif highlight != '' and sentence == '':
+                if i + 1 < len(data):
+                    next_sentence, next_highlight = data[i + 1]
+                    if next_sentence != '' and next_highlight == '':
+                        cleaned_data.append((next_sentence, highlight))
+                        i += 1  # skip the next sentence and highlight as they have been combined
+            elif sentence != '' and highlight != '':
+                cleaned_data.append((sentence, highlight))
+            i += 1
+
+        return cleaned_data
+     #   return list(zip_longest(highlights, comments))
 
     def _parse_highlight(self, annot: fitz.Annot,
                          wordlist: List[Tuple[float, float, float, float, str, int, int, int]]) -> str:
@@ -70,15 +115,44 @@ class ExtractAnnotationsFromPDF:
     def _extract_highlights_from_page(self, page):
         """ Extract all highlights from a page"""
         wordlist = page.get_text("words")  # list of words on page
-        wordlist.sort(key=lambda w: (w[3], w[0]))  # ascending y, then x
+        wordlist.sort(key=lambda w: (w[1], w[0]))  # ascending y, then x
+       # print(wordlist)
 
         highlights = []
-        annot = page.first_annot
+        annot = page.first_annot #order is based on what highlights were added FIRST.
         while annot:
             if annot.type[0] == 8:
                 highlights.append(self._parse_highlight(annot, wordlist))
             annot = annot.next
-        return highlights
+
+        # Extract all annotations from the page
+        annotations = page.annots()
+
+        # Sort annotations by the y-coordinate of the top-left corner of their bounding boxes
+        sorted_annotations = sorted(annotations, key=lambda annot: annot.rect[3])
+
+      #  print([self._parse_highlight(annot, wordlist) for annot in sorted_annotations]) #PROBABLY FIX!
+
+        scenario_string = ' '.join(word_tuple[4] for word_tuple in wordlist)
+        
+        return [self._parse_highlight(annot, wordlist) for annot in sorted_annotations]
+
+    # def _extract_highlights_from_page(self, page):
+    #     """ Extract all highlights from a page"""
+    #     highlights = []
+
+    #     # Extract highlights from annotations
+    #     annot = page.first_annot
+    #     while annot:
+    #         if annot.type[0] == 8:
+    #             highlights.append(self._parse_highlight(annot))
+    #         annot = annot.next
+
+    #     # Sort highlights based on their vertical position (y-coordinate)
+    #     highlights.sort(key=lambda h: h['bounding_box'][1])  # Sort by the y-coordinate of the top-left corner
+
+    #     return highlights
+
 
     @staticmethod
     def select_files_with_consent(input_path, output_path):
@@ -132,7 +206,7 @@ def process_texts(folder_path, start_page): # Process complete texts
     return full_texts
 
 
-def pair_highlight_to_text(annotations): # Separate the highlights based on the texts they are from
+def pair_highlight_to_text(annotations): # Separate the highlights based on the texts they are from TODO: Add participant_ids
     for annotation in annotations:
          # For now: remove annotations not linked to a sentence.. TODO fix this.
         annotation['annotations'] = [ann for ann in annotation['annotations'] if ann[0] is not None]
@@ -143,25 +217,31 @@ def pair_highlight_to_text(annotations): # Separate the highlights based on the 
         title = item['title'].strip()
         if title not in separated_annotations:
             # Initialize separate lists for i[0] and i[1]
-            separated_annotations[title] = {'sentences': [], 'comments': []}
+            separated_annotations[title] = {'sentences': [], 'comments': []} #participants_ids
         
         # Append i[0] and i[1] to their respective lists
         separated_annotations[title]['sentences'].extend([i[0] for i in item['annotations']])
         separated_annotations[title]['comments'].extend([i[1] for i in item['annotations']])
+    #    separated_annotations[title]['participants_ids'].extend([item['participant_id'] for _ in item['annotations']])
+
+
 
     for title, sentences in separated_annotations.items():
         # Split each sentence into separate items based on '.'
             split_sentences = []
             split_comments = []
+       #     split_participant_ids =
 
             for i, sentence in enumerate(sentences['sentences']):
                 # Split text into sentences
-                split_items = [s.strip().replace("\\", "") + '.' for s in sentence.split('.') if s.strip()]
+                split_items = [s.strip().replace("\\", "") for s in sentence.split('.')]
                  # Remove very short annotations.
                 split_items = [item for item in split_items if item and not (len(item)<5)]
                 split_sentences.extend(split_items)
                 # Add comments for each sentence
                 split_comments.extend([sentences['comments'][i]] * len(split_items))
+     #           split_participant_ids.extend([sentences['comments'][i]] * len(split_items))
+
 
             separated_annotations[title]['sentences'] = split_sentences
             separated_annotations[title]['comments'] = split_comments
@@ -201,8 +281,14 @@ def show_word_clouds(separated_annotations, stop_words): # Present word clouds f
         # plt.show()
 
 
-def html_input(title,text, separated_annotations): # Generate input for HTML, including the list of sentences, highlight count, and comments 
-         sentence_list = [s.strip() + '.' for s in ''.join(text.splitlines()).split('.') if s.strip()]
+def html_input(title,text, separated_annotations): # Generate input for HTML, including the list of sentences, highlight count, and comments
+        #  print(text) 
+        #  sentence_list = [s.strip() + '.' for s in ''.join(text.splitlines()).split('.') if s.strip()]
+         # Process the text with SpaCy
+         doc = nlp(text)
+        # Extract sentences
+         sentence_list = [sent.text.strip() for sent in doc.sents]
+
          highlight_counts = np.zeros(len(sentence_list))
         # Create empty sublists using a list comprehension
          comments_indices = [[] for _ in range(len(sentence_list))]
@@ -210,8 +296,9 @@ def html_input(title,text, separated_annotations): # Generate input for HTML, in
          for annotation_index in range(len(separated_annotations[title]['sentences'])):
             for i in range(len(sentence_list)):
              if separated_annotations[title]['sentences'][annotation_index] in sentence_list[i]:
-                 highlight_counts[i] += 1
-                 comments_indices[i].append(annotation_index)
+                 if separated_annotations[title]['comments'][annotation_index] not in [separated_annotations[title]['comments'][index] for index in comments_indices[i]]:
+                    highlight_counts[i] += 1
+                    comments_indices[i].append(annotation_index)
 
          comments_output = []
          for i, sentence in enumerate(sentence_list):
@@ -225,7 +312,48 @@ def html_input(title,text, separated_annotations): # Generate input for HTML, in
          return sentence_list, highlight_counts, comments_output
 
 
-def generate_html(title, sentences, annotations, comments):     # Generate HTML code for the title
+# def generate_html(title, sentences, annotations, comments):     # Generate HTML code for the title
+#     html_title = f'<h1>{title}</h1>'
+    
+#     # Generate HTML code for the sentences with highlighted background and black text
+#     html_sentences = []
+#     for sentence, num_annotations in zip(sentences, annotations):
+#         # Calculate the brightness of the color based on the number of annotations
+#         brightness = int((num_annotations / max(annotations)) * 255)
+#         # Convert brightness to hexadecimal color code for highlight
+#         highlight_color = "#{:02x}{:02x}{:02x}".format(255, 255-brightness, 255-brightness)  # White to red gradient
+        
+#         # Generate HTML code for the sentence with highlighted background and black text
+#         html_sentence = f'<div><span style="background-color:{highlight_color};">{sentence}</span></div>'
+#         html_sentences.append(html_sentence)
+    
+#     # Concatenate HTML-coded sentences into a single HTML string
+#     html_output = html_title + '<div class="heatmap">' + ''.join(html_sentences) + '</div>'
+    
+#     # Add whitespace between sections
+#     html_output += '<br><br>'
+    
+#     # Add subtitle for comments
+#     html_output += '<h2>Comments on the highlighted areas</h2>'
+    
+#     # Generate HTML code for the comments for each sentence
+#     for i, sentence_comments in enumerate(comments):
+#         sentence_comment = sentence_comments[0]
+#         num_times_marked = sentence_comments[1]
+#         additional_comments = sentence_comments[2]
+
+#         # Add section for each sentence
+#         html_output += f'<div><h3>{i+1}. Sentence: {sentence_comment}</h3>'
+#         html_output += f'<p>Times marked: {num_times_marked}</p>'
+#         html_output += '<p>Comments:</p><ul>'
+#         for comment in additional_comments:
+#             html_output += f'<li>{comment}</li>'
+#         html_output += '</ul></div><br>'
+    
+#     return html_output
+
+def generate_html(title, sentences, annotations, comments):
+    # Generate HTML code for the title
     html_title = f'<h1>{title}</h1>'
     
     # Generate HTML code for the sentences with highlighted background and black text
@@ -237,11 +365,11 @@ def generate_html(title, sentences, annotations, comments):     # Generate HTML 
         highlight_color = "#{:02x}{:02x}{:02x}".format(255, 255-brightness, 255-brightness)  # White to red gradient
         
         # Generate HTML code for the sentence with highlighted background and black text
-        html_sentence = f'<div><span style="background-color:{highlight_color};">{sentence}</span></div>'
+        html_sentence = f'<span style="background-color:{highlight_color};">{sentence}</span>'
         html_sentences.append(html_sentence)
     
     # Concatenate HTML-coded sentences into a single HTML string
-    html_output = html_title + '<div class="heatmap">' + ''.join(html_sentences) + '</div>'
+    html_output = html_title + '<div class="heatmap">' + ' '.join(html_sentences) + '</div>'
     
     # Add whitespace between sections
     html_output += '<br><br>'
@@ -256,7 +384,7 @@ def generate_html(title, sentences, annotations, comments):     # Generate HTML 
         additional_comments = sentence_comments[2]
 
         # Add section for each sentence
-        html_output += f'<div><h3>{i+1}. Sentence:{sentence_comment}</h3>'
+        html_output += f'<div><h3>{i+1}. Sentence: {sentence_comment}</h3>'
         html_output += f'<p>Times marked: {num_times_marked}</p>'
         html_output += '<p>Comments:</p><ul>'
         for comment in additional_comments:
@@ -265,24 +393,41 @@ def generate_html(title, sentences, annotations, comments):     # Generate HTML 
     
     return html_output
 
+def create_dataframe(annotations):
+    # Flatten the annotations
+    flattened_annotations = []
+    for d in annotations:
+        for annotation in d['annotations']:
+            flattened_annotations.append({'participant_id': d['participant_id'],
+                                        'title_scenario': d['title'],
+                                        'annotation_sentence': annotation[0],
+                                        'annotation_comment': annotation[1]})
+
+    # Create DataFrame
+    df = pd.DataFrame(flattened_annotations)
+    return df
+
 
 
 if __name__ == '__main__':
+    output_dir = "output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     analyser = ExtractAnnotationsFromPDF('input')
     annotations = analyser.extract_annotations(exclude=[52])
     print(annotations)
+    info_dataframe =  create_dataframe(annotations)
+    info_dataframe.to_csv('output/scenario_dataframe.csv', index=False)
     separated_annotations = pair_highlight_to_text(annotations)
     full_texts = process_texts('input/', 3)
     stop_words = set(stopwords.words('english'))          # Define stopwords to be removed
   #  show_word_clouds(separated_annotations, stop_words)
-    output_dir = "output"
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
 
 
 
     for title, text in full_texts.items():
         sentence_list, highlight_counts, comments_output = html_input(title, text, separated_annotations)
+
         html_output = generate_html(title, sentence_list, highlight_counts, comments_output).encode('ascii', 'ignore').decode()
 
         html_filename = os.path.join(output_dir, f"{title}.html")
